4f874eff93711f225ed30d27bb36f42e4b97b8bd
andev009 <andev@foxmail.com>
2019-01-05 14:29:31 +0800

    add watermark

--- app/src/main/java/com/andev/androidshaderdemo/activity/WaterMarkActivity.java
+++ app/src/main/java/com/andev/androidshaderdemo/activity/WaterMarkActivity.java
@@ -2,7 +2,6 @@ package com.andev.androidshaderdemo.activity;
 
 
 import android.app.Activity;
-import android.graphics.Bitmap;
 import android.os.Bundle;
 
 import com.andev.androidshaderdemo.R;
@@ -11,11 +10,6 @@ import com.andev.androidshaderdemo.widget.CameraView;
 public class WaterMarkActivity extends Activity implements CameraView.OnCameraStateListener{
 
 	CameraView cameraView;
-
-	Bitmap textBitmap;
-
-	int textTextureWidth = 150;
-	int textTextureHeight = 50;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
